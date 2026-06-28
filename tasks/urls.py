65e845e11b96495f1d179f4b2a25002058262523
Oleksandr Averkin <oleksandr.a@devpower.pro>
2026-06-28 19:41:24 +0300

    added auth and task editiing

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("", views.TaskListView.as_view(), name='task-list'),
     path("task/<int:pk>", views.TaskDetailView.as_view(), name='task-detail'),
     path("delete/<int:pk>", views.TaskDeleteView.as_view(), name='task-delete'),
+    path("update/<int:pk>", views.TaskUpdateView.as_view(), name="task-update"),
+    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("register/", views.CustomRegisterView.as_view(), name="register"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from .models import Task
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .forms import TaskFilterForm, TaskCreateForm
+from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Список всіх задач
 class TaskListView(ListView):
@@ -49,3 +52,32 @@ class TaskCreateView(CreateView):
     template_name = "tasks/task_create.html"
     success_url = reverse_lazy("tasks:task-list")
 
+
+# Редагування задачі
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskCreateForm
+    template_name = "tasks/task_update.html"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+# Логаут
+class CustomLogoutView(LogoutView):
+    next_page = 'tasks:task-list'
+
+
+# Логін
+class CustomLoginView(LoginView):
+    template_name = "auth/login.html"
+    redirect_authenticated_user = True
+
+
+# Реєстрація
+class CustomRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "auth/register.html"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(reverse_lazy("tasks:task-list"))
